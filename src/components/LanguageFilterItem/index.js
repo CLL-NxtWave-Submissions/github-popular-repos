63b749e58ed8e43379711d7b6554224624c75ab1
Surya T N <suryatej.nim@yahoo.com>
2022-10-21 06:56:37 +0000

    Implemented JSX elements in LanguageFilterItem component to render the intended UI

--- src/components/LanguageFilterItem/index.js
+++ src/components/LanguageFilterItem/index.js
@@ -1,1 +1,20 @@
-// Write your code here
+import './index.css'
+
+const LanguageFilterItem = props => {
+  const {itemData, itemClickHandler, isSelected} = props
+  const {language} = itemData
+
+  return (
+    <button
+      type="button"
+      className={`language-filter-item-container ${
+        isSelected && 'selected-language'
+      }`}
+      onClick={itemClickHandler}
+    >
+      {language}
+    </button>
+  )
+}
+
+export default LanguageFilterItem
