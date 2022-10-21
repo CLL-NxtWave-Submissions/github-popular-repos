d369bd2cf4796c7c835eba9c5b685485c49b7978
Surya T N <suryatej.nim@yahoo.com>
2022-10-21 13:29:49 +0000

    Implemented JSX elements in RepositoryItem to render the intended UI

--- src/components/RepositoryItem/index.js
+++ src/components/RepositoryItem/index.js
@@ -4,5 +4,30 @@ const RepositoryItem = props => {
   const {itemData} = props
   const {avatarUrl, name, starsCount, forksCount, issuesCount} = itemData
 
-  return <li className="repository-item-bg-container"></li>
+  return (
+  <li className="repository-item-bg-container">
+  {
+    <img className="repository-item-avatar-img" src={avatarUrl} alt="avatar" />
+    <p className="repository-item-name">{name}</p>
+    <ul className="repository-item-stats-list">
+        <li className="repository-item-stat-container">
+            <img className="repository-item-stat-icon-img" src="https://assets.ccbp.in/frontend/react-js/stars-count-img.png" alt="stars" />
+            <p className="repository-item-stat-data">{`${starsCount} stars`}</p>
+        </li>
+
+        <li className="repository-item-stat-container">
+            <img className="repository-item-stat-icon-img" src="https://assets.ccbp.in/frontend/react-js/forks-count-img.png" alt="forks" />
+            <p className="repository-item-stat-data">{`${forksCount} forks`}</p>
+        </li>
+
+        <li className="repository-item-stat-container">
+            <img className="repository-item-stat-icon-img" src="https://assets.ccbp.in/frontend/react-js/issues-count-img.png" alt="open issues" />
+            <p className="repository-item-stat-data">{`${issuesCount} open issues`}</p>
+        </li>
+    </ul>
+  }
+  </li>
+  )
 }
+
+export default RepositoryItem
